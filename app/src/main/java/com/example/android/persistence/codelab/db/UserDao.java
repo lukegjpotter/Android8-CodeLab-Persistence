e1dae61942075605738579b735207f25259c55c3
lukegjpotter <lukegjpotter@gmail.com>
2017-11-16 21:10:34 +0000

    Step 1 - Fetching data with DAO

--- app/src/main/java/com/example/android/persistence/codelab/db/UserDao.java
+++ app/src/main/java/com/example/android/persistence/codelab/db/UserDao.java
@@ -51,7 +51,8 @@ public interface UserDao {
     @Delete
     void deleteUsers(User user1, User user2);
 
-    @Query("SELECT * FROM User WHERE :age == :age") // TODO: Fix this!
+    // TODO: Fix this!
+    @Query("SELECT * FROM User WHERE age < :age")
     List<User> findUsersYoungerThan(int age);
 
     @Query("SELECT * FROM User WHERE age < :age")
